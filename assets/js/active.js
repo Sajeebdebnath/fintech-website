f81f8501f0bafae6b4a24e34b634a8a54d90be97
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-05-31 17:24:32 +0600

    frontend task 2

---
+++ assets/js/active.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".hero-area-wrapper").owlCarousel({
+    loop: true,
+    nav: true,
+    autoPlay: true,
+    items: 1,
+    dotsEach: true,
+    navText: ["<", ">"],
+  })
+})
